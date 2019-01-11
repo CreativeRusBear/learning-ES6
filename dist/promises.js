474dfea2579630f4ec3a65e47775f531b65dae68
Artem <gusev2014russia@mail.ru>
2019-01-11 20:29:09 +0300

    Добавлена работа с promise

---
+++ dist/promises.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function applyForVisa(document) {
+    console.log('Обработка документов...');
+    var promise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            Math.random() > .5 ? resolve({}) : reject('Отказано в визе. Нехватка документов');
+        }, 2000);
+    });
+    return promise;
+};
+
+function getVisa(visa) {
+    console.info('Виза получена');
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            return resolve(visa);
+        }, 2000);
+    });
+}
+function bookHotel(visa) {
+    console.log(visa);
+    console.log('Бронируем номер в отеле');
+    return Math.random() > .5 ? Promise.resolve(visa) : Promise.reject('Нет мест');
+}
+
+function buyTickets(booking) {
+    console.log('Покупаем билеты на смамолет');
+    console.log('Бронирование', booking);
+}
+
+applyForVisa({})
+//далее
+.then(getVisa).then(bookHotel).then(buyTickets)
+//если где-то произойдет ошибка, сработает catch
+.catch(function (error) {
+    return console.error(error);
+});
